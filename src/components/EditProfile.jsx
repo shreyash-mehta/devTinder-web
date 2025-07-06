1145e316127595d71f171190953c3e314232109d
EdenHazardd <ritikmehta123@gmail.com>
2025-07-07 01:26:40 +0530

    feat: Connection Page

---
+++ src/components/EditProfile.jsx
@@ -0,0 +1,145 @@
+import { useState } from "react";
+import UserCard from "./UserCard";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+
+const EditProfile = ({ user }) => {
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
+  const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
+  const [age, setAge] = useState(user.age);
+  const [gender, setGender] = useState(user.gender);
+  const [about, setAbout] = useState(user.about);
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const [showToast, setShowToast] = useState(false);
+
+  const saveProfile = async () => {
+    setError("");
+    try {
+      const res = await axios.patch(
+        BASE_URL + "/profile/edit",
+        {
+          firstName,
+          lastName,
+          photoUrl,
+          age,
+          gender,
+          about,
+        },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res?.data?.data));
+      setShowToast(true);
+      setTimeout(() => {
+        setShowToast(false);
+      }, 3000);
+    } catch (err) {
+      setError(err?.response?.data);
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex justify-center my-10">
+        <div className="flex justify-center mx-10">
+          <div className="card bg-base-300 w-96 shadow-sm">
+            <div className="card-body">
+              <h2 className="card-title justify-center">Edit Profile</h2>
+              <div>
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">First Name:</legend>
+                  <input
+                    type="text"
+                    value={firstName}
+                    className="input"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </fieldset>
+
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">Last Name:</legend>
+                  <input
+                    type="text"
+                    value={lastName}
+                    className="input"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </fieldset>
+
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">Photo URL:</legend>
+                  <input
+                    type="text"
+                    value={photoUrl}
+                    className="input"
+                    onChange={(e) => setPhotoUrl(e.target.value)}
+                  />
+                </fieldset>
+
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">Age:</legend>
+                  <input
+                    type="text"
+                    value={age}
+                    className="input"
+                    onChange={(e) => setAge(e.target.value)}
+                  />
+                </fieldset>
+
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">Gender:</legend>
+
+                  <select
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                    className="input"
+                  >
+                    <option value="">Select gender</option>
+                    <option value="male">male</option>
+                    <option value="female">female</option>
+                    <option value="other">other</option>
+                  </select>
+                </fieldset>
+
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">About:</legend>
+                  <input
+                    type="text"
+                    value={about}
+                    className="input"
+                    onChange={(e) => setAbout(e.target.value)}
+                  />
+                </fieldset>
+              </div>
+
+              <div className="card-actions justify-center">
+                <div>
+                  <p className="text-red-500">{error}</p>
+                </div>
+                <button className="btn btn-primary my-3" onClick={saveProfile}>
+                  Save Profile
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <UserCard
+          user={{ firstName, lastName, photoUrl, age, gender, about }}
+        />
+      </div>
+      {showToast && (
+        <div className="toast toast-top toast-center">
+          <div className="alert alert-success">
+            <span>Profile saved successfully.</span>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default EditProfile;
---
+++ src/utils/connectionSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const connectionSlice = createSlice({
+  name: "connection",
+  initialState: null,
+  reducers: {
+    addConnnection: (state, action) => {
+      return action.payload;
+    },
+    removeConnection: (state, action) => {
+      return null;
+    },
+  },
+});
+
+export default connectionSlice.reducer;
+export const {addConnnection, removeConnection} = connectionSlice.actions;

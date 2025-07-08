6d8637872c9f9da9ec1357e07e1213a4ec5c49a2
shreyash-mehta <shreyashmehta001@gmail.com>
2025-07-09 02:22:25 +0530

    feat: show connection requests

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,40 +1,43 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addConnnection } from "../utils/connectionSlice";
+import { addRequest } from "../utils/requestSlice";
+import { useEffect } from "react";
 
-const Connections = () => {
-  const connections = useSelector((store) => store.connections);
+const Requests = () => {
+  const requests = useSelector((store) => store.requests);
   const dispatch = useDispatch();
-  const fetchConnections = async () => {
+
+  const fetchRequests = async () => {
     try {
-      const res = await axios.get(BASE_URL + "/user/connections", {
+      const res = await axios.get(BASE_URL + "/user/requests/received", {
         withCredentials: true,
       });
-      console.log(res.data.data);
-      dispatch(addConnnection(res.data.data));
+
+      dispatch(addRequest(res?.data?.data));
     } catch (err) {}
   };
+
   useEffect(() => {
-    fetchConnections();
+    fetchRequests();
   }, []);
 
-  if (!connections) return;
+  if (!requests) return;
 
-  if (connections.length === 0) return <h1> No Connections Found</h1>;
+  if (requests.length === 0) return <h1> No Requests Found</h1>;
 
   return (
     <div className="text-center my-10">
-      <h1 className="text-bold text-white text-3xl">Connections</h1>
+      <h1 className="text-bold text-white text-3xl">Connection Requests</h1>
 
-      {connections.map((connection) => {
+      {requests.map((request) => {
         const { _id, firstName, lastName, photoUrl, age, gender, about } =
-          connection;
+          request.fromUserId;
+
         return (
           <div
             key={_id}
-            className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto"
+            className="flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-2/3 mx-auto"
           >
             <div>
               <img
@@ -50,6 +53,10 @@ const Connections = () => {
               <p>{about}</p>
               {age && gender && <p>{age + ", " + gender}</p>}
             </div>
+            <div>
+              <button className="btn btn-primary m-2">Reject</button>
+              <button className="btn btn-secondary m-2">Accept</button>
+            </div>
           </div>
         );
       })}
@@ -57,4 +64,4 @@ const Connections = () => {
   );
 };
 
-export default Connections;
+export default Requests;
